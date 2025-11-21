47ac8bc31a8a76865cc8720796ed913b4d5aa428
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-21 15:16:54 -0800

    Extend grid to 16x16
    Also adjusted CSS to allow grid to scale with
    the window.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function createGrid () {
-    for (let i = 0; i < 16; i++) {
-        const container = document.querySelector(".container");
-        const box = document.createElement("div");
-        container.appendChild(box);
-    }
+for (let i = 0; i < 256; i++) {
+    const container = document.querySelector(".container");
+    const box = document.createElement("div");
+    container.appendChild(box);
 }
 
-createGrid();
