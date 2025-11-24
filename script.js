5b6494fbee7e4aed9a6412ba6dd660601102db7e
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-24 11:35:54 -0800

    Add more user options and basic instructions
    Added options for the user to clear the grid
    without resetting to default dimensions, and to
    change the "pen" color.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 drawGrid();
 
-const resize = document.querySelector(".canvas-size");
+const resize = document.querySelector(".grid-size");
+const clear = document.querySelector(".clear");
+
 resize.addEventListener("click", () => {
     let dimensions;
     const boxesPerSide = prompt("How many squares per side? Enter a whole number from 8 to 100.");
-    if (!boxesPerSide) dimensions = 16
+    if (boxesPerSide === "") dimensions = 16
     else if (boxesPerSide < 8) dimensions = 8
     else if (boxesPerSide > 100) dimensions = 100
     else dimensions = boxesPerSide;
-    drawGrid(dimensions);
+    if (!(boxesPerSide === null)) drawGrid(dimensions);
+})
+
+clear.addEventListener("click", () => {
+    const squares = document.querySelectorAll("div div");
+    squares.forEach((square) => square.style.backgroundColor = "white")
 })
 
 function drawGrid(dimension = 16) {
     const container = document.querySelector(".container");
+    const colorSelector = document.querySelector(".color-selector");
     container.innerHTML = "";
+    colorSelector.value = "#000000";
     for (let i = 0; i < (dimension * dimension); i++) {
         const box = document.createElement("div");
         container.appendChild(box);
@@ -21,13 +30,13 @@ function drawGrid(dimension = 16) {
     const boxDimensions = document.querySelectorAll("div div");
     boxDimensions.forEach((box) => {
         box.setAttribute("style", `border: 0.01em solid black; 
-            width: ${(Math.round((100/dimension) * 100)) / 100}%;
-            height: ${(Math.round((100/dimension) * 100)) / 100}%;`)
+            width: ${(Math.floor((100/dimension) * 100)) / 100}%;
+            height: ${(Math.floor((100/dimension) * 100)) / 100}%;`)
     });
     const squares = document.querySelectorAll("div div");
     squares.forEach((square) => {
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "black";
+            square.style.backgroundColor = colorSelector.value;
         })
     })
 }
