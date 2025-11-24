b36dbb431225020308385b91f811e3f98c2d9779
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2025-11-24 09:58:34 -0800

    Add feature to resize the grid
    When user presses the "RESIZE THE GRID" button,
    prompt will accept a value from 8 to 100 and
    reconstruct the grid with that many boxes per
    side.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-for (let i = 0; i < 256; i++) {
-    const container = document.querySelector(".container");
-    const box = document.createElement("div");
-    container.appendChild(box);
-}
+drawGrid();
 
-const squares = document.querySelectorAll("div div");
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    })
+const resize = document.querySelector(".canvas-size");
+resize.addEventListener("click", () => {
+    let dimensions;
+    const boxesPerSide = prompt("How many squares per side? Enter a whole number from 8 to 100.");
+    if (!boxesPerSide) dimensions = 16
+    else if (boxesPerSide < 8) dimensions = 8
+    else if (boxesPerSide > 100) dimensions = 100
+    else dimensions = boxesPerSide;
+    drawGrid(dimensions);
 })
+
+function drawGrid(dimension = 16) {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    for (let i = 0; i < (dimension * dimension); i++) {
+        const box = document.createElement("div");
+        container.appendChild(box);
+    }
+    const boxDimensions = document.querySelectorAll("div div");
+    boxDimensions.forEach((box) => {
+        box.setAttribute("style", `border: 0.01em solid black; 
+            width: ${(Math.round((100/dimension) * 100)) / 100}%;
+            height: ${(Math.round((100/dimension) * 100)) / 100}%;`)
+    });
+    const squares = document.querySelectorAll("div div");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        })
+    })
+}
